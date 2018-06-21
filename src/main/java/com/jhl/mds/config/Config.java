8b54503b80b49c801450ae99c4637fda1fadc107
minh.trannguyenhao <minh.trannguyenhao@lazada.com>
2018-06-21 16:18:08 +0800

    switch main db storage to sqlite

--- src/main/java/com/jhl/mds/config/Config.java
+++ src/main/java/com/jhl/mds/config/Config.java
@@ -1,14 +1,30 @@
 package com.jhl.mds.config;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import javax.validation.Valid;
 
 @Configuration
 public class Config {
 
+    @Value("${spring.datasource.url:jdbc:sqlite:mds.db}")
+    private String sqliteDatabaseFile;
+
     @Bean
     public LayoutDialect layoutDialect() {
         return new LayoutDialect();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.sqlite.JDBC");
+        dataSourceBuilder.url(sqliteDatabaseFile);
+        return dataSourceBuilder.build();
     }
 }
