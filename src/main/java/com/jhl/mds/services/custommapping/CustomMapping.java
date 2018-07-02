e2bf0a8f3b8d083eae202237d76df3fe7e221e14
minh.trannguyenhao <minh.trannguyenhao@lazada.com>
2018-07-02 14:03:50 +0700

    update custom mapping with better implementation

--- src/main/java/com/jhl/mds/services/custommapping/CustomMapping.java
+++ src/main/java/com/jhl/mds/services/custommapping/CustomMapping.java
@@ -1,45 +1,31 @@
 package com.jhl.mds.services.custommapping;
 
-import com.jhl.mds.util.Regex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import javax.script.Bindings;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
-import java.util.List;
 import java.util.Map;
 
 @Service
 public class CustomMapping {
 
-    private static final String MARKER_PREFIX = "\\$\\$\\$MARKER_";
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public String resolve(String input, Map<String, Object> data) throws ScriptException {
-        // replace string with marker
-        List<String> list = Regex.findAllStringMatches(input, "'[^']+'");
-        for (int i = 0; i < list.size(); i++) {
-            input = input.replaceAll(list.get(i), MARKER_PREFIX + i);
-        }
-        for (Map.Entry<String, Object> e : data.entrySet()) {
-            Object value = e.getValue();
-            if (value instanceof Number) {
-                input = input.replaceAll(e.getKey(), String.valueOf(e.getValue()));
-            } else {
-                input = input.replaceAll(e.getKey(), "'" + String.valueOf(e.getValue()) + "'");
-            }
-        }
-        // replace marker with string
-        for (int i = 0; i < list.size(); i++) {
-            input = input.replaceAll(MARKER_PREFIX + i, list.get(i));
-        }
         ScriptEngineManager manager = new ScriptEngineManager();
         ScriptEngine engine = manager.getEngineByName("js");
 
+        Bindings bindings = engine.createBindings();
+        for (Map.Entry<String, Object> e : data.entrySet()) {
+            bindings.put(e.getKey(), e.getValue());
+        }
+
         logger.info("Try to evaluation " + input);
-        Object result = engine.eval(input);
+        Object result = engine.eval(input, bindings);
         return String.valueOf(result);
     }
 }
