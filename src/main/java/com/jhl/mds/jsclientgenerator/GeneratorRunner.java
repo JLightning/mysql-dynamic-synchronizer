b36439ca151eb6b15f740d0b6b3b5ccf19d2b261
minh.trannguyenhao <minh.trannguyenhao@lazada.com>
2018-10-10 17:02:44 +0700

    remove js generator runner from main application

--- src/main/java/com/jhl/mds/jsclientgenerator/GeneratorRunner.java
+++ src/main/java/com/jhl/mds/jsclientgenerator/GeneratorRunner.java
@@ -1,12 +1,15 @@
 package com.jhl.mds.jsclientgenerator;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
 @Service
+@Profile("js-generator")
 public class GeneratorRunner {
 
     private JsClientGenerator jsClientGenerator;
@@ -27,7 +30,8 @@ public class GeneratorRunner {
     }
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(JsClientGenerator.class);
+        args = ArrayUtils.add(args, "--spring.profiles.active=js-generator");
+        ConfigurableApplicationContext ctx = SpringApplication.run(JsClientGenerator.class, args);
         ctx.close();
     }
 }
